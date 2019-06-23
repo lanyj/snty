f477be4457d74aed14699fd64976ec75101b774b
Jay Lan <403678693@qq.com>
2019-06-24 01:46:21 +0800

    override default hashCode, equals and toString method

--- src/main/java/cn/lanyj/snty/common/msg/AbstractMessage.java
+++ src/main/java/cn/lanyj/snty/common/msg/AbstractMessage.java
@@ -49,4 +49,47 @@ public abstract class AbstractMessage<T extends Serializable> implements Message
 	public void setStatus(MessageStatus status) {
 		this.status = status;
 	}
+
+	@Override
+	public int hashCode() {
+		if (id() == null) {
+			return super.hashCode();
+		}
+		return id().hashCode();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean equals(Object obj) {
+		boolean ret = true;
+		if (obj instanceof Message) {
+			if (!obj.getClass().equals(this.getClass())) {
+				return false;
+			}
+			Message<T> op = (Message<T>) obj;
+			if (op.id() == null || this.id() == null) {
+				ret &= op.id() == this.id();
+			} else {
+				ret &= op.id().equals(this.id());
+			}
+			if (op.content() == null || this.content() == null) {
+				ret &= op.content() == this.content();
+			} else {
+				ret &= op.content().equals(this.content());
+			}
+			if (op.status() == null || this.status() == null) {
+				ret &= op.status() == this.status();
+			} else {
+				ret &= op.status().equals(this.status());
+			}
+		} else {
+			ret = false;
+		}
+		return ret;
+	}
+
+	@Override
+	public String toString() {
+		return "[" + id() + ", " + status() + ", " + content() + "]";
+	}
 }
